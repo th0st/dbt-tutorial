1b2fedeb272ebcc86f2f0c3d486d9788c413e8ea
Thomas Strauven <t.strauven@web.de>
2025-04-23 18:48:48 +0200

    switch back

--- jaffle_shop/models/customers.sql
+++ jaffle_shop/models/customers.sql
@@ -1,6 +1,4 @@
-with 
-    /*
-    customers as (
+with customers as (
 
     select
         id as customer_id,
@@ -37,9 +35,9 @@ customer_orders as (
     group by 1
 
 ),
-    */
+
 final as (
-    /*
+
     select
         customers.customer_id,
         customers.first_name,
@@ -51,10 +49,7 @@ final as (
     from customers
 
     left join customer_orders using (customer_id)
-    */
-    select *
-    from `dbt-tutorial.jaffle_shop.orders`
-    where id = 94
+
 )
 
 select * from final
